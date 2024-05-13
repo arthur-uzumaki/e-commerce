c5da6ed0b91e2ddd411eeff2706bdd0ef3acf1dc
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-05-12 22:12:02 -0300

    :feat: criação da pagina home com as images

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,4 +1,67 @@
+import Image from 'next/image'
+import Link from 'next/link'
 export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
-  return <h1>Hello word!</h1>
+  return (
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href="/"
+        className="relative group col-span-6 row-span-6 flex justify-center items-end bg-zinc-900 rounded-lg overflow-hidden"
+      >
+        <Image
+          src="/moletom.png"
+          width={920}
+          height={920}
+          alt=""
+          quality={100}
+          className=" group-hover:scale-105 transition-transform duration-500"
+        />
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 pl-5 ">
+          <span className="text-sm truncate ">Moletom Never</span>
+          <span className="bg-violet-500 flex items-center  h-full rounded-full justify-center px-4 text-center font-semibold ">
+            R$129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="relative group col-span-3 row-span-3 flex justify-center items-end bg-zinc-900 rounded-lg overflow-hidden"
+      >
+        <Image
+          src="/moletom-java.png"
+          width={920}
+          height={920}
+          alt=""
+          quality={100}
+          className=" group-hover:scale-105 transition-transform duration-500"
+        />
+        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 pl-5 ">
+          <span className="text-sm truncate ">Moletom Never</span>
+          <span className="bg-violet-500 flex items-center  h-full rounded-full justify-center px-4 text-center font-semibold ">
+            R$129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className=" relative group col-span-3 row-span-3 flex justify-center items-end bg-zinc-900 rounded-lg overflow-hidden"
+      >
+        <Image
+          src="/moletom-ia-p-devs.png"
+          width={920}
+          height={920}
+          alt=""
+          quality={100}
+          className=" group-hover:scale-105 transition-transform duration-500"
+        />
+        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 pl-5 ">
+          <span className="text-sm truncate ">Moletom Never Stop Learning</span>
+          <span className="bg-violet-500 flex items-center  h-full rounded-full justify-center px-4 text-center font-semibold ">
+            R$129
+          </span>
+        </div>
+      </Link>
+    </div>
+  )
 }
