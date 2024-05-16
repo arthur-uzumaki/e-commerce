fd598d4d00214acab859a8535a741130beb50497
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-05-16 16:24:48 -0300

    feat: criação de metadata na páginas

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { Product } from '@/data/@types/products'
 import { api } from '@/data/api'
+import { Metadata } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -17,6 +18,10 @@ async function getFeaturedProducts(): Promise<Product[]> {
   })
   const products = await response.json()
   return products
+}
+
+export const metadata: Metadata = {
+  title: 'Home',
 }
 export default async function Home() {
   await new Promise((resolve) => setTimeout(resolve, 1000))
