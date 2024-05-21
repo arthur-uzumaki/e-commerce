03b3e872c6635ed0b65f2ae03851a4b31061687c
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-05-21 11:28:37 -0300

    feat: conectando a busca da api com a página de search

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,109 +1,78 @@
+import { Product } from '@/data/@types/products'
+import { api } from '@/data/api'
 import Image from 'next/image'
 import Link from 'next/link'
-export default async function Search() {
+import { redirect } from 'next/navigation'
+
+interface SearchProps {
+  searchParams: {
+    q: string
+  }
+}
+
+async function searchProducts(query: string): Promise<Product[]> {
+  const revalidateOneHour = 60 * 60
+  const response = await api(`/products/search?q=${query}`, {
+    next: {
+      revalidate: revalidateOneHour,
+    },
+  })
+  const products = await response.json()
+  return products
+}
+
+export default async function Search({ searchParams }: SearchProps) {
+  const { q: query } = searchParams
+
+  if (!query) {
+    redirect('/')
+  }
+
+  const products = await searchProducts(query)
   return (
     <div className="flex flex-col gap-4">
       <p className="text-sm">
         {' '}
-        Resultado para: <span className="font-semibold">moletom</span>
+        Resultado para: <span className="font-semibold">{query}</span>
       </p>
       <div className="grid grid-cols-3 gap-6 ">
-        <Link
-          href={`/product/moletom-never-stop-learning`}
-          className="relative group  flex items-end justify-center
+        {products.map((product) => {
+          return (
+            <Link
+              key={product.id}
+              href={`/product/${product.slug}`}
+              className="relative group  flex items-end justify-center
              bg-zinc-900 rounded-lg overflow-hidden"
-        >
-          <Image
-            src="/camiseta-dowhile-2022.png"
-            width={480}
-            height={480}
-            quality={100}
-            alt=""
-            className="group-hover:scale-105 transition-transform duration-500"
-          />
-
-          <div
-            className="absolute bottom-16  right-10 h-12 flex items-center
-             gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 pl-5 "
-          >
-            <span className="text-sm truncate ">Camiseta dowhile</span>
-            <span
-              className="bg-violet-500 flex items-center  h-full 
-              rounded-full justify-center px-4 text-center font-semibold "
             >
-              {Number(200).toLocaleString('pt-br', {
-                style: 'currency',
-                currency: 'BRL',
-                maximumFractionDigits: 0,
-                minimumFractionDigits: 0,
-              })}
-            </span>
-          </div>
-        </Link>
-        <Link
-          href={`/product/moletom-never-stop-learning`}
-          className="relative group  flex items-end justify-center
-             bg-zinc-900 rounded-lg overflow-hidden"
-        >
-          <Image
-            src="/camiseta-dowhile-2022.png"
-            width={480}
-            height={480}
-            quality={100}
-            alt=""
-            className="group-hover:scale-105 transition-transform duration-500"
-          />
+              <Image
+                src={product.image}
+                width={480}
+                height={480}
+                quality={100}
+                alt=""
+                className="group-hover:scale-105 transition-transform duration-500"
+              />
 
-          <div
-            className="absolute bottom-16  right-10 h-12 flex items-center
+              <div
+                className="absolute bottom-16  right-10 h-12 flex items-center
              gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 pl-5 "
-          >
-            <span className="text-sm truncate ">Camiseta dowhile</span>
-            <span
-              className="bg-violet-500 flex items-center  h-full 
+              >
+                <span className="text-sm truncate ">{product.title}</span>
+                <span
+                  className="bg-violet-500 flex items-center  h-full 
               rounded-full justify-center px-4 text-center font-semibold "
-            >
-              {Number(200).toLocaleString('pt-br', {
-                style: 'currency',
-                currency: 'BRL',
-                maximumFractionDigits: 0,
-                minimumFractionDigits: 0,
-              })}
-            </span>
-          </div>
-        </Link>
-        <Link
-          href={`/product/moletom-never-stop-learning`}
-          className="relative group  flex items-end justify-center
-             bg-zinc-900 rounded-lg overflow-hidden"
-        >
-          <Image
-            src="/camiseta-dowhile-2022.png"
-            width={480}
-            height={480}
-            quality={100}
-            alt=""
-            className="group-hover:scale-105 transition-transform duration-500"
-          />
-
-          <div
-            className="absolute bottom-16  right-10 h-12 flex items-center
-             gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 pl-5 "
-          >
-            <span className="text-sm truncate ">Camiseta dowhile</span>
-            <span
-              className="bg-violet-500 flex items-center  h-full 
-              rounded-full justify-center px-4 text-center font-semibold "
-            >
-              {Number(200).toLocaleString('pt-br', {
-                style: 'currency',
-                currency: 'BRL',
-                maximumFractionDigits: 0,
-                minimumFractionDigits: 0,
-              })}
-            </span>
-          </div>
-        </Link>
+                >
+                  {product.price.toLocaleString('pt-br', {
+                    style: 'currency',
+                    currency: 'BRL',
+                    maximumFractionDigits: 0,
+                    minimumFractionDigits: 0,
+                  })}
+                </span>
+              </div>
+            </Link>
+          )
+        })}
       </div>
     </div>
   )
