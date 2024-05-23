6556de1f5eb6443862d0ac0948728217ec8390c5
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-05-23 15:25:50 -0300

    ci: create cypress e2e worflow

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,16 +1,15 @@
-'use client'
+import { Suspense } from 'react'
+
 import { Skeleton } from '@/components/skeleton'
-import { useSearchParams } from 'next/navigation'
+import { CurrentSearch } from './currentSearch'
 
 export default function SearchLoading() {
-  const searParams = useSearchParams()
-
-  const query = searParams.get('q')
   return (
     <div className="flex flex-col gap-4">
-      <p>
-        Resultado para:<span className="font-semibold">{query}</span>
-      </p>
+      <Suspense fallback={null}>
+        <CurrentSearch />
+      </Suspense>
+
       <div className="grid grid-cols-3 gap-6">
         <Skeleton className="h-[400px]" />
         <Skeleton className="h-[400px]" />
